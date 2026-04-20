4c8cc9a60f889ed6138972c1cc0a6976d295e621
deaklevi <deakleventevok@gmail.com>
2026-04-20 14:46:22 +0200

    change nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'NeoLogic | Webfejlesztés',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/NeoLogic logo.ico' }
+      ],
+      meta: [
+        { name: 'description', content: 'A NeoLogic valóra váltja céged digitális transzformációját. Kifinomult web fejlesztés, logikusan felépített design.' },
+        { name: 'keywords', content: 'szoftverfejlesztés, Vue.js, NeoLogic, digitális megoldások, weboldal készítés' },
+        { name: 'author', content: 'NeoLogic Team' },
+        // Open Graph
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'NeoLogic | Modern Szoftvermegoldások' },
+        { property: 'og:description', content: 'Egyedi szoftverfejlesztés és modern webes megoldások. Nézd meg, miben segíthetünk!' },
+        { property: 'og:image', content: 'https://neologicdemo.saavatar.xyz/NeoLogic-logo.ico' },
+      ],
+    }
+  }
 })
