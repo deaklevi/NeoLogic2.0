01daccdee22dcb4170c515c14d10479b21fed42f
deaklevi <deakleventevok@gmail.com>
2026-04-21 12:50:21 +0200

    Add projekt new logo adn favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/NeoLogic logo.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       ],
       meta: [
         { name: 'description', content: 'A NeoLogic valóra váltja céged digitális transzformációját. Kifinomult web fejlesztés, logikusan felépített design.' },
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
         { property: 'og:type', content: 'website' },
         { property: 'og:title', content: 'NeoLogic | Modern Szoftvermegoldások' },
         { property: 'og:description', content: 'Egyedi szoftverfejlesztés és modern webes megoldások. Nézd meg, miben segíthetünk!' },
-        { property: 'og:image', content: 'https://neologic.hu/NeoLogic%20logo.ico' },
+        { property: 'og:image', content: 'https://neologic.hu/favicon.ico' },
       ],
     }
   }
